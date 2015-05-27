1049d8c9b458e562d4a4f1c3c1b1ca4f113fa584
Luke Maser <lukemaser@gmail.com>
2015-05-27 16:57:58 -0400

    Initial commit, sharing with Riley for debugging.

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	// Questions
+	var Questions = [{
+		question: "When were doughnuts invented?",
+		multipleChoice: ["500 B.C.", "1922", "Mid 1800s"],
+		correctChoice: 2
+	},
+	{
+		question: "What famous children's book features a doughnut making machine?",
+		multipleChoice: [" Homer Price by Robert Mcloskey", " Where the Wild Things Are ", " Amelia Bedelia "],
+		correctChoice: 0
+	},
+	{
+		question: "How many doughnuts are consumed by Americans each year?",
+		multipleChoice: ["A Baker's Dozen", "500 million", "Over ten billion"],
+		correctChoice: 2
+	},
+	{
+		question: "What are doughnuts called in Poland?",
+		multipleChoice: ["Paczkis (pronounced punch-key)", "Polish Donuts", "doughnuts"],
+		correctChoice: 0
+	},
+	{
+		question: "How many doughnuts are ",
+		multipleChoice: ["answer", "choice", "maybe"],
+		correctChoice: 1
+	}]
+
+	var currentQuestion = 0;
+
+	$("#controls").on('click', '#submit', function(e) {
+		e.preventDefault();
+		console.log("click");
+		currentQuestion++;
+		nextQuestion();
+		console.log(currentQuestion);
+	});
+
+	function nextQuestion() {
+		if(currentQuestion < 5) {
+			$(".question").remove();
+			$("#answer-wrapper input").remove();
+			$("#answer-wrapper span").remove();
+			var newAnswer1 = '<span class="question">'+Questions[currentQuestion].question+'</span><div id="answer-wrapper"><input type="radio" name="Answer" class="answer-select" value="0" /><span class="answer-one">'+Questions[currentQuestion].multipleChoice[0]+'</span></br><input type="radio" name="Answer" class="answer-select" value="1"/><span class="answer-two">'+Questions[currentQuestion].multipleChoice[1]+'</span></br><input type="radio" name="Answer" class="answer-select" value="2"/><span class="answer-three">'+Questions[currentQuestion].multipleChoice[2]+'</span></br></div>';
+			// <div id="controls"><input id="submit" type="button" value="Submit Answer"/></div>';
+			$(".container").html(newAnswer1);
+		}
+		else {
+			$(".question").remove();
+			$("#answer-wrapper input").remove();
+			$("#answer-wrapper span").remove();
+			var qLast = '<p>Good Job! Would you like to try again?</p><div id="controls"><input type="button", id="new", value="Retry?"/>';
+		};
+	}
+});
