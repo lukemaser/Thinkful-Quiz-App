f615de834cff1db50059b632c538fd465198e737
Luke Maser <lukemaser@gmail.com>
2015-05-30 11:10:42 -0400

    Added Submit, Next, and New game sequence. Updated Questions and trivia.

--- js/app.js
+++ js/app.js
@@ -3,52 +3,110 @@ $(document).ready(function() {
 	var Questions = [{
 		question: "When were doughnuts invented?",
 		multipleChoice: ["500 B.C.", "1922", "Mid 1800s"],
-		correctChoice: 2
+		questionNum: 0,
+		correctChoice: 2,
+		trivia: "Legend has it that the hole in the middle originated from a sailor punching the middle out of the pastries his mother made because they weren't cooked completely."
 	},
 	{
 		question: "What famous children's book features a doughnut making machine?",
-		multipleChoice: [" Homer Price by Robert Mcloskey", " Where the Wild Things Are ", " Amelia Bedelia "],
-		correctChoice: 0
+		multipleChoice: [" Homer Price", " Where the Wild Things Are", " Amelia Bedelia"],
+		questionNum: 1,
+		correctChoice: 0,
+		trivia: "Homer Price was turned into a musical."
 	},
 	{
 		question: "How many doughnuts are consumed by Americans each year?",
-		multipleChoice: ["A Baker's Dozen", "500 million", "Over ten billion"],
-		correctChoice: 2
+		multipleChoice: ["A Baker's Dozen", "500 something", "Over ten billion"],
+		questionNum: 2,
+		correctChoice: 2,
+		trivia: "Surprisingly, this is not a problem."
 	},
 	{
 		question: "What are doughnuts called in Poland?",
-		multipleChoice: ["Paczkis (pronounced punch-key)", "Polish Donuts", "doughnuts"],
-		correctChoice: 0
+		multipleChoice: ["Paczkis (pronounced poonch-key)", "Polish Donuts", "Go nuts"],
+		questionNum: 3,
+		correctChoice: 0,
+		trivia: "Paczki's are consumed by the dozens the week before lent. (As well as all kinds of junk food.)"
 	},
 	{
-		question: "How many doughnuts are ",
-		multipleChoice: ["answer", "choice", "maybe"],
-		correctChoice: 1
+		question: "What state has an  official state doughnut?",
+		multipleChoice: ["Nebraska", "Louisiana", "Ohio"],
+		questionNum: 4,
+		correctChoice: 1,
+		trivia: "The Beignet (ben-yay), is the official state doughnut of Louisiana."
 	}]
 
 	var currentQuestion = 0;
+	var currentScore = 0;
 
 	$("#controls").on('click', '#submit', function(e) {
 		e.preventDefault();
-		console.log("click");
+		keepScore();
+		showTrivia();
+
 		currentQuestion++;
-		nextQuestion();
-		console.log(currentQuestion);
+		
 	});
+
+	$("#controls").on('click', '#next', function(e) {
+		e.preventDefault();
+		nextQuestion();
+		$("#next").css("display", "none");
+		$("#submit").css("display", "inline");
+	});
+
+	$('#controls').on('click', '#new', function(e) {
+		currentScore = 0;
+		currentQuestion = 0;
+		e.preventDefault();
+		nextQuestion();
+		var newAnswer1 = '<h2 class="question"><span>'+Questions[currentQuestion].question+'</span></h2><input type="radio" name="Answer" class="answer-select" value="0" /><span class="answer-one">'+Questions[currentQuestion].multipleChoice[0]+'</span></br><input type="radio" name="Answer" class="answer-select" value="1"/><span class="answer-two">'+Questions[currentQuestion].multipleChoice[1]+'</span></br><input type="radio" name="Answer" class="answer-select" value="2"/><span class="answer-three">'+Questions[currentQuestion].multipleChoice[2]+'</span>';
+		$(".question-wrapper").html(newAnswer1);
+		$("#new").css("display", "none");
+		$("#submit").css("display", "inline");
+	});
+
+	// Track user's score //
+	function keepScore() {
+		var answer = $("input[type='radio']:checked").val();
+		if (answer == Questions[currentQuestion].correctChoice) {
+			currentScore++;
+			console.log("correct");
+		}
+	}
+	function showTrivia() {
+		$(".question").remove();
+		$("#question-wrapper input").remove();
+		$("#question-wrapper span").remove();
+		var triviaDisplay = '<h2 class=question>'+Questions[currentQuestion].trivia+'</h2>';
+		console.log("currrent?");
+		$("#question-wrapper").html(triviaDisplay);
+		$("#next").css("display", "inline");
+		$("#submit").css("display", "none");
+	}
 
 	function nextQuestion() {
 		if(currentQuestion < 5) {
 			$(".question").remove();
-			$("#answer-wrapper input").remove();
-			$("#answer-wrapper span").remove();
-			var newAnswer1 = '<span class="question">'+Questions[currentQuestion].question+'</span><div id="answer-wrapper"><input type="radio" name="Answer" class="answer-select" value="0" /><span class="answer-one">'+Questions[currentQuestion].multipleChoice[0]+'</span></br><input type="radio" name="Answer" class="answer-select" value="1"/><span class="answer-two">'+Questions[currentQuestion].multipleChoice[1]+'</span></br><input type="radio" name="Answer" class="answer-select" value="2"/><span class="answer-three">'+Questions[currentQuestion].multipleChoice[2]+'</span>';
-			$("#answer-wrapper").html(newAnswer1);
+			$("#question-wrapper input").remove();
+			$("#question-wrapper span").remove();
+			var newAnswer1 = '<h2 class="question"><span>'+Questions[currentQuestion].question+'</span></h2><input type="radio" name="Answer" class="answer-select" value="0" /><span class="answer-one">'+Questions[currentQuestion].multipleChoice[0]+'</span></br><input type="radio" name="Answer" class="answer-select" value="1"/><span class="answer-two">'+Questions[currentQuestion].multipleChoice[1]+'</span></br><input type="radio" name="Answer" class="answer-select" value="2"/><span class="answer-three">'+Questions[currentQuestion].multipleChoice[2]+'</span>';
+			$("#question-wrapper").html(newAnswer1);
 		} else {
 			$(".question").remove();
-			$("#answer-wrapper input").remove();
-			$("#answer-wrapper span").remove();
-			var qLast = '<p>Good Job! Would you like to try again?</p><div id="controls"><input type="button", id="new", value="Retry?"/>';
-			$(".container").html(qLast);
+			$("#question-wrapper input").remove();
+			$("#question-wrapper span").remove();
+			$("#new").css("display", "inline");
+			$("#submit").css("display", "none");
+			if (currentScore >= 4){
+				var qLast = '<p>Cool.</br>You got ' +currentScore+' out of 5 correct.</br> Congratulate yourself with a doughnut.</br>Want to try again?</p>';
+				$("#question-wrapper").html(qLast);
+			} else if (currentScore <= 4) {
+				var qLast = '<p>Cool.</br>You got ' +currentScore+' Correct.</br> Console yourself with a doughnut.</br>Want to try again?</p>';
+				$("#question-wrapper").html(qLast);
+			}
+			
+			// <div id="controls"><input type="button", id="new", value="New Game?"/></div>'
 		};
 	}
 });
